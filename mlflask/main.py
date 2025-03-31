fe9228aa5a022a5e8f727261134027877475b3b2
mobaison <mobaison21@gmail.com>
2025-03-31 12:21:50 +0530

    Update main.py

--- mlflask/main.py
+++ mlflask/main.py
@@ -49,6 +49,5 @@ def predict_upload():
     predicted_label = predict(img)
     return jsonify({'prediction': predicted_label})
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
