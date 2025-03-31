9431e19f7d495bb670efe3f29986d61b78d6e0b9
mobaison <mobaison21@gmail.com>
2025-03-31 12:22:20 +0530

    Update main.py

--- mlflask/main.py
+++ mlflask/main.py
@@ -2,32 +2,25 @@ from flask import Flask, render_template, request, jsonify
 import cv2
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 from PIL import Image
 import base64
 import io
 import os
 
 app = Flask(__name__)
+model = load_model("model2.h5")  # Ensure the correct model path
 
-model = load_model("model2.h5") 
 def preprocess_image(image):
     """Preprocess image for model prediction"""
     img = cv2.resize(image, (28, 28))
     img = img.astype("float32") / 255.0
-    img = np.expand_dims(img, axis=(0, -1))  # Reshape for model
+    img = np.expand_dims(img, axis=(0, -1))
     return img
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def predict(image):
-    """Runs inference using TensorFlow Lite"""
-    processed_img = preprocess_image(image)
-    interpreter.set_tensor(input_details[0]['index'], processed_img)
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    return int(np.argmax(output_data))
 
 @app.route('/predict_canvas', methods=['POST'])
 def predict_canvas():
@@ -35,7 +28,9 @@ def predict_canvas():
     img_data = base64.b64decode(data.split(',')[1])
     img = Image.open(io.BytesIO(img_data)).convert("L")
     img = np.array(img)
-    predicted_label = predict(img)
+    processed_img = preprocess_image(img)
+    prediction = model.predict(processed_img)
+    predicted_label = int(np.argmax(prediction))
     return jsonify({'prediction': predicted_label})
 
 @app.route('/predict_upload', methods=['POST'])
@@ -44,9 +39,11 @@ def predict_upload():
         return jsonify({'error': 'No file uploaded'})
     file = request.files['file']
     img = Image.open(file).convert("L")
-    img = img.resize((28, 28))
+    img = img.resize((28, 28))  # Ensure correct resizing
     img = np.array(img)
-    predicted_label = predict(img)
+    processed_img = preprocess_image(img)
+    prediction = model.predict(processed_img)
+    predicted_label = int(np.argmax(prediction))
     return jsonify({'prediction': predicted_label})
 
 if __name__ == '__main__':
